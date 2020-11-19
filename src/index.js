b7fed23cb6cbfb8a711e1826947953b5b3e1a838
Juan Verdugo <juanverdugobc@gmail.com>
2020-11-18 19:26:10 -0700

    Making a route file

--- src/index.js
+++ src/index.js
@@ -2,20 +2,29 @@ const express = require('express')
 const app = express();
 const path = require('path')
 
-//* SETTINGS
+// const indexRoute = require('./routes/index')
+
+//* settings
 app.set('port', 3000)
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
 
-//* MIDDLEWARES
+//* middlewares
 
 
-//* ROUTES
-app.get('/', (req, res) => {
+//* routes
+/*app.get('/', (req, res) => {
     // res.sendFile('D://Documents/nodeProjects/node-firstWebsite/src/')
     // console.log(__dirname + '/views/index.html')
-    console.log(path.join(__dirname, 'views/index.html'))
-    res.sendFile(path.join(__dirname, 'views/index.html'))
-})
+    // console.log(path.join(__dirname, 'views/index.html'))
+    // res.sendFile(path.join(__dirname, 'views/index.ejs')) /* Archivo HTML
+    res.render('index', {title: 'First Website'})
+})*/
+
+// app.use(indexRoute)
+
+app.use(require('./routes/'))
 
 
 //* STATIC FILES
