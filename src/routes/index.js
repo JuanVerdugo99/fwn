567b042b499c6222e00b01c942b824b508a231e7
Juan Carlos Verdugo Salazar <juanverdugobc@gmail.com>
2020-11-20 11:26:49 -0700

    UMA

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router()
 
 
 router.get('/', (req, res) => {
-    res.render('index.html', {title: 'First Website'})
+    res.render('index.html', {title: 'UMA'})
 })
 
 router.get('/contact', (req, res) => {
